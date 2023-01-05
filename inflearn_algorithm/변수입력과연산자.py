b096e000199fc068145a6aec27348e41115a1c7b
earlysummer0303 <jw9914@naver.com>
2023-01-05 11:46:13 +0900

    파이썬 알고리즘 학습을 위해 파이썬 문법 복습하기
    :w

--- inflearn_algorithm/변수입력과연산자.py
+++ inflearn_algorithm/변수입력과연산자.py
@@ -26,3 +26,12 @@ print("int 형변환 해서 합하면 ->",c)
 a,b,c = map(int,input().split())
 print(a+b+c)
 
+# 파이썬 사칙연산
+
+print("합 : ", a+b)
+print("차 : ", a-b)
+print("곱 : ", a*b)
+print("나눗셈의 결과 : ", a/b)
+print("몫 : ", a//b)
+print("나머지 : ", a%b)
+print("거듭제곱 : ",a**b) ## a를 b번 곱한 값 (즉 a의 b제곱)
